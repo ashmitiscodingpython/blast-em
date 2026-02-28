0606107ee1092d78a079f28e20843183c5644118
ashmitiscodingpython <ashmit.jeevan@gmail.com>
2026-02-28 22:46:25 +0530

    Yeah, today I did some things

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,19 +8,33 @@ var input = Vector2()
 @export var accel := 1200.0
 @export var friction := 1400.0
 @onready var ground = $"../Main"
+var layers = []
+var on_stair = false
 var wep = Vector2()
 var input_dir
 var z = 0
 
-func get_tile_data():
-	var tile = ground.local_to_map(global_position)
-	var data: TileData = ground.get_cell_tile_data(tile)
+func layer_no():
+	var data
+	var laye
+	for layer in layers:
+		data = get_tile_data(layer)
+		if data:
+			laye = layer
+			break
+	return laye
+	
+
+func get_tile_data(layer:TileMapLayer):
+	var tile = layer.local_to_map(global_position)
+	var data: TileData = layer.get_cell_tile_data(tile)
 	return data
 
 func direction(target: Vector2):
 	return atan2((position.y - target.y), (position.x - target.x))
 
 func _ready() -> void:
+	layers = [$"../Main", $"../Layer 1 Plateus"]
 	Input.set_custom_mouse_cursor(preload("res://kenney_desert-shooter-pack_1.0/PNG/Weapons/Tiles/tile_0035.png"))
 
 func _physics_process(delta: float) -> void:
@@ -33,9 +47,14 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _process(_delta: float) -> void:
-	var tileda = get_tile_data()
-	if tileda: 
-		print(tileda.get_custom_data("Stair"))
+	var tileda = get_tile_data(ground)
+	var layer = layer_no()
+	int(layer.get_groups()[0])
+	if tileda:
+		if tileda.get_custom_data("Stair") and !on_stair:
+			on_stair = true
+		elif !tileda.get_custom_data("Stair"):
+			on_stair = false
 	if input_dir.x < 0:
 		sprite.scale.x = -1
 	elif input_dir.x > 0:
