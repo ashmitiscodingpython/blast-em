039b0877afbe19b2afa3bf0ccf0a7195a868c310
ashmitiscodingpython <ashmit.jeevan@gmail.com>
2026-03-28 23:35:07 +0530

    Highest hours (yet) in a day!
    I got 3h 49m and that's my highest so far! Anyway, I finished that stupid highlighting system but I didn't do the proper triggering yet :heavysob: I'll do that tomorrow

---
+++ scripts/highlight.gd
@@ -0,0 +1,43 @@
+extends Sprite2D
+
+@export var highlighting = false
+var highlight_start = 0
+var time = 0
+var repeats = 75
+var repeating = false
+var timer_done = false
+
+func _ready() -> void:
+	set_instance_shader_parameter("radius", 18)
+
+func _process(delta: float) -> void:
+	time += delta
+	if repeating:
+		if repeats < 1:
+			repeating = false
+			highlighting = false
+			timer_done = false
+			set_instance_shader_parameter("overriding", false)
+			set_instance_shader_parameter("radius", 18)
+		else:
+			repeats -= 1
+			var gettt = get_instance_shader_parameter("override")
+			set_instance_shader_parameter("override", gettt + ((0 - gettt) / 5.0))
+	if highlighting:
+		visible = true
+		var gett = get_instance_shader_parameter("radius")
+		if (gett - 0.115) < 0.01:
+			if !timer_done:
+				timer_done = true
+				$Timer.start()
+		set_instance_shader_parameter("radius", gett + ((0.115 - gett) / 5))
+	else:
+		visible = false
+		highlight_start = 0
+		set_instance_shader_parameter("radius", 18)
+
+func callback():
+	repeating = true
+	repeats = 75
+	set_instance_shader_parameter("override", 1)
+	set_instance_shader_parameter("overriding", true)
